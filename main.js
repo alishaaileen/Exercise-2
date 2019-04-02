7bdba3f4da6e4bbcffe67327957b16550f0bdacb
Alisha Aileen <33864105+alishaileen@users.noreply.github.com>
2019-04-02 23:34:36 +0700

    fixed operator & length bugs

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-// var display = document.querySelector('.display').value
-
 // inputan angka
 function insert(inputan){
-    // var display = document.querySelector('.display').value
-    if(document.querySelector('.display').value === "0"){
-        document.querySelector('.display').value = ""
-    }
+    var display = document.querySelector('.display')
+    var panjang = display.value.length
+    var temp = display.value.charAt(panjang-1)
 
-    if(inputan === '÷'){
-        document.querySelector('.display').value = document.querySelector('.display').value + '/'
-    }
-    else if(inputan === '×'){
-        document.querySelector('.display').value = document.querySelector('.display').value + '*'
-    }
-    else{
-        document.querySelector('.display').value = document.querySelector('.display').value + inputan
+    if(panjang <= 15){      // batas panjang karakter agar tidak terlalu panjang
+        if(display.value === "0"){  // Jika display kalkulator memang baru "0" saja
+            display.value = ""      // tidak akan menambah angka 0 jika diinput 0 lagi
+        }
+    
+        if(temp === "+" || temp === "-" || temp === "/" || temp === "*"){ // mengecek jika akhirannya operator
+            if(typeof inputan === "number"){            // hanya bisa input kalau inputan selanjutnya angka
+                display.value = display.value + inputan // agar penulisan operator tidak terjadi 2 kali berturut-turut
+            }
+        }
+        else{
+            if(inputan === '÷'){
+                display.value = display.value + '/' // biar bisa dihitung menggunakan fungsi eval()
+            }
+            else if(inputan === '×'){
+                display.value = display.value + '*' // biar bisa dihitung menggunakan fungsi eval()
+            }
+            else{
+                display.value = display.value + inputan
+            }
+        }
     }
 }
 
-// reset
+// reset atau AC
 
 var reset = document.querySelector('.reset')
 
@@ -31,14 +41,14 @@ reset.onclick = () => {
 var backspace = document.querySelector('.backspace')
 
 backspace.onclick = () => {
-    var display = document.querySelector('.display').value
-    var panjang = display.length
+    var display = document.querySelector('.display')
+    var panjang = display.value.length
 
-    if(panjang === 1){
-        document.querySelector('.display').value = "0"
+    if(panjang === 1){      // Jika menghapus angka terakhir
+        display.value = "0" // atau memang 0, display-nya 0
     }
     else{
-        document.querySelector('.display').value = display.substr(0, panjang-1)
+        display.value = display.value.substr(0, panjang-1)
     }
 }
 
@@ -47,10 +57,14 @@ backspace.onclick = () => {
 var result = document.querySelector('#result')
 
 result.onclick = () => {
-    var display = document.querySelector('.display').value
-    var hasil = eval(display)
+    var display = document.querySelector('.display')
+    var hasil = eval(display.value)
+    var temp = hasil.toString() // Agar bisa memakai .length
 
-    console.log(hasil)
-
-    document.querySelector('.display').value = hasil
+    if(temp.length > 15){ // Agar hasil tidak melebihi display
+        display.value = "too long!"
+    }
+    else{
+        display.value = hasil
+    }
 }
